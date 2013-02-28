c54bfc056b5aa4941aec51d09d569835f974a3f0
Luiz Antonio S Messias <tonyzrp@gmail.com>
2013-02-28 19:55:58 -0300

    commit inicial, app gerado pelo bake

---
+++ Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'root',
+		'database' => 'crudusuarios',
+		'encoding' => 'utf8'
+	);
+}
